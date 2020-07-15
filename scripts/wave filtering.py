606ccc6608389f257fd435ab78b00b87d03e765d
Nico van Bentum <niico0708@gmail.com>
2020-07-15 23:28:57 +0200

    removed unnecessary code

--- scripts/wave filtering.py
+++ scripts/wave filtering.py
@@ -4,6 +4,7 @@ It showcases multiple filtering techniques and shows what they do to the signal.
 """
 import io as io
 import numpy as np
+import math as math
 import wave as wave
 from PIL import ImageTk, Image
 from scipy import signal as sp
@@ -13,19 +14,17 @@ import tkinter.filedialog
 import tkinter.messagebox
 import tkinter.scrolledtext
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.backend_bases import key_press_handler
 
 plt.rcParams.update({'figure.autolayout': True})
 
-def fig2img(fig):
-    """Convert a Matplotlib figure to a PIL Image and return it"""
-    import io
-    buf = io.BytesIO()
-    fig.savefig(buf)
-    buf.seek(0)
-    img = Image.open(buf)
-    return img.copy()
+
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret / n
 
 
 class Application:
@@ -55,34 +54,40 @@ class Application:
         self.wavFile = None
         self.timeData = None
         self.freqData = None
-    
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.window)
-        self.canvas.draw()
-        self.canvas.get_tk_widget().grid(row=0, column=0)
-
-        frame = tk.Frame(self.window)
-        frame.grid(row=1, column=0)
-        self.toolbar = NavigationToolbar2Tk(self.canvas, frame)
-        self.toolbar.update()
 
         # init frames
-        self.median_kernel_size = tk.Scale(self.window, from_=3, to=103, variable=3, orient=tk.HORIZONTAL, label="Median Kernel Size")
-        self.median_kernel_size.grid(row=0, column=1, columnspan=2, sticky=(tk.W, tk.E))
+        kernel_med_size = tk.StringVar()
+        self.median_slider = tk.Scale(
+            self.window, from_=3, to=103, variable=kernel_med_size, orient=tk.HORIZONTAL, label="Median Kernel")
+        self.median_slider.grid(
+            row=0, column=0, columnspan=2, sticky=(tk.W, tk.E, tk.N))
+
+        kernel_ma_size = tk.StringVar()
+        self.move_ave_slider = tk.Scale(self.window, from_=3, to=103, variable=kernel_ma_size, orient=tk.HORIZONTAL, label="Moving Average Kernel")
+        self.move_ave_slider.grid(row=1, column=0, columnspan=2, sticky=(tk.W, tk.E, tk.N))
 
         self.current_combo_item = tk.StringVar()
-        self.filter_combo_box = ttk.Combobox(
-            self.window, width=27, textvariable=self.current_combo_item)
-        self.filter_combo_box['values'] = ('Median', 'Low-pass', 'High-pass')
-        self.filter_combo_box.current(0)
-        self.filter_combo_box.grid(row=1, column=1, columnspan=2)
+        self.current_combo_item.set('Median')
+        self.filter_combo_box = tk.OptionMenu(self.window, self.current_combo_item, 'Median', 'Moving average')
+        self.filter_combo_box.grid(row=2, column=0, columnspan=2, sticky=(tk.W, tk.E))
 
         self.filter_btn = tk.Button(self.window, text="filter",
                                     command=self.onFilter, padx=2, pady=2)
-        self.filter_btn.grid(row=2, column=1, columnspan=1, sticky=(tk.W, tk.E))
+        self.filter_btn.grid(
+            row=3, column=0, columnspan=1, sticky=(tk.W, tk.E))
 
         self.reset_btn = tk.Button(self.window, text="reset",
                                    command=self.reset, padx=2, pady=2)
-        self.reset_btn.grid(row=2, column=2, columnspan=1, sticky=(tk.W, tk.E))
+        self.reset_btn.grid(row=3, column=1, columnspan=1, sticky=(tk.W, tk.E))
+        
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.window)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=0, column=2, rowspan=3)
+
+        frame = tk.Frame(self.window)
+        frame.grid(row=3, column=2)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, frame)
+        self.toolbar.update()
 
     def init_menu(self):
         menubar = tk.Menu(self.window)
@@ -91,22 +96,34 @@ class Application:
         menubar.add_cascade(label="File", menu=filemenu)
         return menubar
 
+    
+    def filterFrequency(self):
+        if self.freqData is None:
+            return
+
+
     def onFilter(self):
         if self.timeData is None or self.freqData is None:
             return
 
         filteredTimeData = None
         filteredFreqData = None
-        if self.filter_combo_box.get() == 'Median':
-            odd_kernel_size = self.median_kernel_size.get() // 2 * 2 + 1
+
+        odd_kernel_size = self.median_slider.get() // 2 * 2 + 1
+
+        if self.current_combo_item.get() == 'Median':
             filteredTimeData = sp.medfilt(self.timeData, odd_kernel_size)
             filteredFreqData = sp.medfilt(self.freqData, odd_kernel_size)
-            self.median_kernel_size.set(odd_kernel_size)
+            self.median_slider.set(odd_kernel_size)
 
+        elif self.current_combo_item.get() == 'Moving average':
+            filteredTimeData = moving_average(self.timeData, self.move_ave_slider.get())
+            filteredFreqData = self.freqData = abs(np.fft.rfft(filteredTimeData))
         else:
             return
 
-        self.ax[0].plot(np.arange(self.wavFile.getnframes())/self.wavFile.getframerate(), filteredTimeData)
+        self.ax[0].plot(np.arange(self.wavFile.getnframes()) /
+                        self.wavFile.getframerate(), filteredTimeData)
         self.ax[1].plot(filteredFreqData)
         self.canvas.draw()
         self.toolbar.update()
@@ -125,7 +142,8 @@ class Application:
         self.ax[1].set_ylabel('|amplitude|')
 
         if self.timeData is not None and self.freqData is not None:
-            self.ax[0].plot(np.arange(self.wavFile.getnframes())/self.wavFile.getframerate(), self.timeData)
+            self.ax[0].plot(np.arange(self.wavFile.getnframes()) /
+                            self.wavFile.getframerate(), self.timeData)
             self.ax[1].plot(self.freqData)
             self.canvas.draw()
             self.toolbar.update()
@@ -146,7 +164,8 @@ class Application:
         self.timeData = self.timeData[0::2]
         self.freqData = abs(np.fft.rfft(self.timeData))
 
-        self.ax[0].plot(np.arange(self.wavFile.getnframes())/self.wavFile.getframerate(), self.timeData)
+        self.ax[0].plot(np.arange(self.wavFile.getnframes()) /
+                        self.wavFile.getframerate(), self.timeData)
         self.ax[1].plot(self.freqData)
 
         self.canvas.draw()
@@ -154,6 +173,7 @@ class Application:
 
     def run(self):
         self.window.mainloop()
+
 
 def main():
     app = Application()
